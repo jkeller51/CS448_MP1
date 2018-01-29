b63ef2bb2b6d44a8bc515f374d0bf8c52495edfc
Jon Keller <j.keller51@live.com>
2018-01-29 16:38:47 -0600

    Moved functions to separate file.

--- depth-first.py
+++ depth-first.py
@@ -6,51 +6,16 @@ Created on Mon Jan 29 16:03:19 2018
 """
 
 # commence search with depth first
-import os
-
+import include as gg
 debug=1
 
-def loadmaze(filename):
-    if not (os.path.exists(filename) and os.path.isfile(filename)):
-        print("File not found!")
-    try:
-        file = open(filename)          # error handling
-        
-    except:
-        print("File not found or accessible.")
-        raise
-    else:
-        maze = []                 # initialize maze
-        for line in file:
-            maze.append(line)     # load maze line by line
-            
-        file.close()
-    
-        return maze
-    
-def search(maze):
-    # find start position (P)
-    q=0
-    startpos=(-1,-1)
-    while q<len(maze):
-        i=0
-        while i<len(maze[q]):       # search each character
-            if (maze[q][i] == "P"):   # if it is the starting character "P"
-                startpos = (q,i)     # update startpos
-                break
-            i+=1
-        if (startpos != (-1, -1)):    # we're done here
-            break
-        q+=1
-        
-    if (debug==1):        
-        print(startpos)
+
         
     
     
                 
 
-maze = loadmaze("mediumMaze.txt")
+maze = gg.loadmaze("mediumMaze.txt")
 
-search(maze)
+gg.search(maze)
 
