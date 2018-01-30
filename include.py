2cce4c83be5be95b0650c5903f672e0fe2ce54e0
HaokunLi1994 <35938271+HaokunLi1994@users.noreply.github.com>
2018-01-29 21:46:33 -0600

    Update include.py
    add class of node (currently committed), using (x, y) to imply the position of a particular node

--- include.py
+++ include.py
@@ -17,6 +17,59 @@ class Node(object):
     def __init__(self, cost, wall):
         self.cost = cost
         self.wall = wall
+
+
+'''        
+class Node(object):
+    """ Using coordinates (x, y) to mark the position
+    of this node.
+    """
+    beenthere = 0
+    wall = 0
+    previousNode = None;
+    
+    def __init__(self, x, y, char):
+        self.x = x
+        self.y = y
+        self.value = char
+
+        if char == "%":
+            self.wall = 1
+
+    def find_children(self, maze):
+        children = []
+        width = len(maze[0])
+        height = len(maze)
+        
+        # up next
+        if self.x  > 0:
+            child_char = maze[self.x - 1][self.y]
+            child = Node(self.x - 1, self.y, child_char)
+            children.append(child)
+            
+        # left next
+        if self.y  > 0:
+            child_char = maze[self.x][self.y - 1]
+            child = Node(self.x, self.y - 1, child_char)
+            children.append(child)
+            
+        # right next
+        if self.y + 1  < width:
+            child_char = maze[self.x][self.y + 1]
+            child = Node(self.x, self.y + 1, child_char)
+            children.append(child)
+            
+        # bottom next
+        if self.x + 1 < height:
+            child_char = maze[self.x + 1][self.y]
+            child = Node(self.x + 1, self.y, child_char)
+            children.append(child)
+
+        return children
+
+    def set_previous_node(self, node):
+        self.previousNode = node
+'''
     
 
 def loadmaze(filename):
@@ -40,20 +93,20 @@ def loadmaze(filename):
 
 def find_start(maze):
     # find start position (P)
-    q=0
-    startpos=(-1,-1)
-    while q<len(maze):
-        i=0
-        while i<len(maze[q]):       # search each character
+    q = 0
+    startpos = (-1, -1)
+    while q < len(maze):
+        i = 0
+        while i < len(maze[q]):       # search each character
             if (maze[q][i] == "P"):   # if it is the starting character "P"
-                startpos = (q,i)     # update startpos
+                startpos = (q, i)     # update startpos
                 break
-            i+=1
+            i += 1
         if (startpos != (-1, -1)):    # we're done here
             break
-        q+=1
+        q += 1
         
-    if (debug==1):        
+    if (debug == 1):        
         print(startpos)
     
     return startpos
@@ -61,20 +114,20 @@ def find_start(maze):
 
 def find_end(maze):
     # find goal position (.)
-    q=0
-    endpos=(-1,-1)
-    while q<len(maze):
-        i=0
-        while i<len(maze[q]):       # search each character
+    q = 0
+    endpos = (-1, -1)
+    while q < len(maze):
+        i = 0
+        while i < len(maze[q]):       # search each character
             if (maze[q][i] == "."):   # if it is the starting character "."
-                endpos = (q,i)     # update startpos
+                endpos = (q, i)     # update startpos
                 break
-            i+=1
+            i += 1
         if (endpos != (-1, -1)):    # we're done here
             break
-        q+=1
+        q += 1
         
-    if (debug==1):        
+    if (debug == 1):        
         print(endpos)
         
     return endpos
