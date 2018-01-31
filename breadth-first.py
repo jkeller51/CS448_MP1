c0c6c237f89e8c58f2a3d1818e4213d21ef20f84
HaokunLi1994 <35938271+HaokunLi1994@users.noreply.github.com>
2018-01-30 20:21:27 -0600

    BFS algorithm
    Already passed every test case in less than 1 sec.
    
    Maybe we should only keep the BFS function (and remove main function) in this file?

---
+++ breadth-first.py
@@ -0,0 +1,79 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 15:41:28 2018
+
+@author: HaokunLi1994
+"""
+
+
+import queue
+import include_node_version as inc
+
+
+def BFS(maze, startNode):
+    """ Searching for a path using BFS
+
+    Args:
+        maze(list): list of node objects
+        startNode(Node)
+    Returns:
+        step(int): number of expand steps to find a path from 'P' to '.'
+    """
+    # Initialize fontier queue
+    unvisited = queue.Queue()
+    unvisited.put(startNode)
+
+    # Innitialize cost value
+    startNode.cost = 0
+
+    # Count the number of expand step
+    step = 0
+
+    while not unvisited.empty():
+        current = unvisited.get()
+        current.beenthere = 1
+        step += 1
+
+        # Terminating conditions
+        if current.value == ".":
+            break
+
+        # Update searching frontier
+        children = current.find_children(maze)
+        for child in children:
+
+            # The node is unvisited and it is not in
+            # the queue of unvisited nodes
+            if (child.beenthere == 0) and (child not in unvisited.queue):
+                child.previousNode = current
+                unvisited.put(child)
+                child.cost = child.previousNode.cost + 1
+
+    return step
+
+
+if __name__ == '__main__':
+    # Initialize maze
+    maze = inc.loadmaze('openmaze.txt')
+
+    # Find startNode
+    start_x, start_y = inc.find_start(maze)
+    startNode = maze[start_x][start_y]
+
+    # Find endNode
+    end_x, end_y = inc.find_end(maze)
+    endNode = maze[end_x][end_y]
+
+    # Number of steps
+    step = BFS(maze, startNode)
+
+    # Print
+    print('original maze:')
+    inc.printmaze(maze)
+    print('\nsolved maze:')
+    inc.traceback(maze, endNode)
+
+    # Output
+    print('\nnumber of nodes visited in total: {0}'.format(step))
+    print('cost of path found: {0}'.format(endNode.cost))
