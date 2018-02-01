c8535bcbb2940f4c291e690aa1baf4fc13c8fbcf
Jon Keller <j.keller51@live.com>
2018-01-31 23:05:07 -0600

    Created depth-first branch

--- depth-first.py
+++ depth-first.py
@@ -6,16 +6,36 @@ Created on Mon Jan 29 16:03:19 2018
 """
 
 # commence search with depth first
-import include as gg
-debug=1
+import queue
+import include_node_version as inc
 
+def DFS(maze, startNode):
+    """ Find the first working path using depth-first search
 
+    Args:
+        maze(list): list of node objects
+        startNode(Node)
+    Returns:
+        step(int): number of steps of the found path from 'P' to '.'
+    """
+    
+    # Initialize fontier queue
+    unvisited = queue.Queue()
+    unvisited.put(startNode)
+    
+    # Initialize cost value
+    startNode.cost = 0
+
+    # Count the number of expand step
+    step = 0
+    
+    while not unvisited.empty():
+        current = unvisited.get()
+        current.beenthere = 1
+        step += 1
+
+        # Terminating conditions
+        if current.value == ".":
+            break
         
-    
-    
-                
-
-maze = gg.loadmaze("mediumMaze.txt")
-
-gg.search(maze)
-
+        
