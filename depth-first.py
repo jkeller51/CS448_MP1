35ecc759bd2f46ed49c99e4e1701a07c0229f9c3
Jon Keller <j.keller51@live.com>
2018-02-02 15:29:16 -0600

    Rewrote algorithm
    Now uses loops instead of recursion

--- depth-first.py
+++ depth-first.py
@@ -59,13 +59,43 @@ def DFS(maze, startNode):
     
     # Initialize cost value
     startNode.cost = 0
+    
+    current = startNode
 
     # Count the number of expand step
     step = 0
 
     # begin expanding nodes, starting with the start node
-    step = go_to_child(startNode)
     
+    children = current.find_children(maze)
+    while True:
+        while (children != None):
+            current.beenthere = 1
+            step+=1
+            if current.value == ".":
+                found_solution=1
+                break
+            found_child = 0
+            for i in range(0,len(children)):
+                child = children[i]
+                if child.beenthere == 0:
+                    found_child = 1
+                    break
+            if (found_child == 0):
+                # we've visited all children. nothing more to see here
+                break
+            child.previousNode = current        # so we can trace back later
+            child.cost = child.previousNode.cost + 1
+            current = child
+            children = current.find_children(maze)
+        if (found_solution == 1):
+            break
+        elif current.value != "P":           # check to see we haven't climbed all the way up the tree yet
+            current = current.previousNode
+            children = current.find_children(maze)
+        else:    # this shouldn't happen, but just in case we find no solution
+            break
+            
     return step
             
         
