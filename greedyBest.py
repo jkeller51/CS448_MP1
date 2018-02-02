a8a5f9070c6d1568468026c1b3890d1e820ae423
cemeka <chinny.emeka1@gmail.com>
2018-02-01 23:42:53 -0600

    outline of greedy best first search

---
+++ greedyBest.py
@@ -0,0 +1,6 @@
+#Implementation of greedy best first search
+#Use a heuristic function to determine most promising node and expand accordingly.
+#Use priority queue data structure
+#Heuristic to use is Manhattan's distance.
+
+
