2f694938164a83dede60c319275348d576a92921
cemeka <chinny.emeka1@gmail.com>
2018-02-07 10:19:45 -0600

    Sokoban outline

--- Part2.py
+++ Part2.py
@@ -5,3 +5,14 @@ Created on Tue Feb  6 20:16:40 2018
 @author: jkell
 """
 
+#Code outline:
+#Boxes should be their own data structure
+#Store whether the boxes are in a valid position (B)
+#Store all boxes in puzzle in an array
+#Heuristic (tentative): dist. to nearest displaced box + dist. to box after that
+#or maybe dist. to nearest displaced box + dist. to nearest storage space
+
+#State of world: your position(prob. just current node), the array of boxes.
+#Transition from your position (pos, 8 displaced boxes) to (newpos, 7 displaced boxes) etc.
+
+
---
+++ sokoban_include.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+# Indicating ON/OFF of debugging mode
+debug = 0
+
+
+class Node(object):
+    """ Using coordinates (x, y) to mark the position
+    of this node.
+
+    Args:
+        x(int):x-th line of the maze
+        y(int):y-th position in that line of the maze
+        char(string):character of a particular node
+    """
+    # Indicating whether or not the node has been visited
+    beenthere = 0
+
+    # Indicating whether the node is part of 'wall'
+    wall = 0
+
+    # Parent node
+    previousNode = None
+    
+    def __init__(self, x, y, char):
+        self.x = x
+        self.y = y
+        self.value = char
+        self.cost = None
+        self.evaluation = None
+        self.food_path = []
+
+        if char == "%":
+            self.wall = 1
+
+        pass
+
+    def find_children(self, maze):
+        """ Find all children nodes of a particular node
+
+        Args:
+            maze(list):list of Node objects
+        Returns:
+            children(list):list of children in Node objects
+        """
+        children = []
+        width = len(maze[0])
+        height = len(maze)
+        
+        # up next
+        if self.x  > 0:
+            child = maze[self.x - 1][self.y]
+            if (child.wall == 0) and (child.value != "P"):
+                children.append(child)
+            
+        # left next
+        if self.y  > 0:
+            child = maze[self.x][self.y - 1]
+            if (child.wall == 0) and (child.value != "P"):
+                children.append(child)
+            
+        # right next
+        if self.y + 1  < width:
+            child = maze[self.x][self.y + 1]
+            if (child.wall == 0) and (child.value != "P"):
+                children.append(child)
+            
+        # bottom next
+        if self.x + 1 < height:
+            child = maze[self.x + 1][self.y]
+            if (child.wall == 0) and (child.value != "P"):
+                children.append(child)
+
+        return children
+
+
+    def set_previous_node(self, node):
+        """ Set parent node for current node.
+
+        Args:
+            node(Node): current node
+        Returns:
+            (None)
+        """
+        self.previousNode = node
+        pass
+
+
+    def __eq__(self, other):
+        """
+        Override default equals behavior. 2 nodes are equal if and only if
+        the two nodes have the same location (i.e. same x and y coordinates)
+        """
+        if (self.x == other.x) and (self.y == other.y):
+            return True
+        else:
+            return False
+
+
+
+def loadmaze(filename):
+    """ Load maze into memory
+
+    Args:
+        filename(str):path to a maze file, eg. 'openmaze.txt'
+    Returns:
+        maze(list):list of Node objects
+    """
+    myfile = open(filename)
+    maze = []
+    x = 0
+    for line in myfile:
+        row = []
+        y = 0
+        for char in line:
+            if char == "\n":
+                continue
+
+            row.append(Node(x, y, char))
+            y += 1
+
+        maze.append(row)
+        x += 1
+
+    myfile.close()
+    
+    return maze
+
+
+def printmaze(maze):
+    """ Printing function for mazes
+
+    Args:
+        maze(list):list of Node objects
+    Returns:
+        (None)
+    """
+    out = []
+    for line in maze:
+        row = []
+        for char in line:
+            row.append(char.value)
+        out.append(row)
+
+    for line in out:
+        print(''.join(line))
+
+    pass
+
+
+def find_start(maze):
+    """ Find starting point of a maze
+
+    Args:
+        maze(list):list of Node objects
+    Returns:
+        startpos(tuple):(x, y) coordinates of starting point
+    """
+    for line in maze:
+        for char in line:
+            if char.value == "P":
+                startpos = (char.x, char.y)
+
+                if (debug == 1):
+                    print(startpos)
+                    
+                return startpos
+
+
+
+
+def find_storageLocs(maze):
+      """Find all the places in maze where we should have boxes
+
+      Args:
+        maze(list): list of Node objects
+      Returns storage(list): list of storage locations. 
+         
+      """
+      storage = []
+      for line in maze:
+          for char in line:
+              if char.value  == ".":
+                  storage.append(char) #this is a storage location
+
+      if (debug == 1):
+           print(len(storage))
+      return storage
+  
+
+
+
+def reset_all_nodes(maze):
+    """ Reset status for all nodes in the maze.
+    
+    Args;
+        maze(list): list of nodes
+    Returns:
+        (None)
+    """ 
+    for line in maze:
+        for char in line:
+            char.cost = 0
+            char.beenthere = 0
+            char.previousNode = None
+            char.evaluation = None
+    pass
+
+
+"""
+def traceback(maze, endNode):
+    """ Mark the found path from 'P' to '.' with '.'
+
+    Args;
+        endNode(node): ending point of the maze
+    Returns:
+        (None)
+    """
+    current = endNode
+    previous = current.previousNode
+
+    while previous.value != "P":
+        previous.value = "."
+        current = previous
+        previous = current.previousNode
+
+    printmaze(maze)
+    pass
+"""
+
+
+def heuristic(cur, goal):
+    """
+      Heuristic based on the manhattan distance between the current point and the goal position.
+      Used for best first search      
+    """
+    distance = abs(cur.x - goal.x) + abs(cur.y - goal.y)
+    return distance
+    
